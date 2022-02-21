23577db13241b39a7136c724212ac02c18f20677
xcarceco <xcarceco@gmail.com>
2022-02-21 23:28:13 +0100

    Initial commit

---
+++ Ejercicio1pythonbasico.py
@@ -0,0 +1,9 @@
+# Crea un archivo de python donde imprimas varias cadenas de texto.
+# Las cadenas que debes de imprimir son las siguientes:
+# - Hola, soy [tu_nombre]
+# - Estoy empezando el curso de python
+# - Espero aprender mucho
+
+cursopython='-Hola, soy Xavi\n-Estoy empezando el curso de python\n-Espero aprender mucho'
+
+print(cursopython)
